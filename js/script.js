27c781220deff85fb10b9837f3fe1dd2f3a1c3f6
blackrose <wahomekelving@gmail.com>
2018-12-03 09:11:53 +0300

    added a cunction and variables length

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function getTriangleType(){
+  var length1 = parseInt (document.getElementById('length1').value);
+  var length2 = parseInt (document.getElementById('length2').value);
+  var length3 = parseInt (document.getElementById('length3').value);
+  var result = document.getElementById('result')
+
+  if(length1==length2 $$ length2==length3)
+   alert('This is an Equilateral triangle')
+
+}
