8d61c000dc0bc41b1791244a740fb177d3e6becc
blackrose <wahomekelving@gmail.com>
2018-12-03 10:34:56 +0300

    changed alert method

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function triangleType(){
-  var length1 = parseInt (document.getElementById('length1').value);
-  var length2 = parseInt (document.getElementById('length2').value);
-  var length3 = parseInt (document.getElementById('length3').value);
+  var a = parseInt (document.getElementById('length1').value);
+  var b = parseInt (document.getElementById('length2').value);
+  var c = parseInt (document.getElementById('length3').value);
   var result = document.getElementById('result')
 
   if(length1===length2 && length2===length3){
-    alert('This is an Equilateral triangle')
+    document.getElementById('This is an Equilateral triangle')
 
   }
    else if(lenth1!== length2 && length2=== length3){
-     alert("This is an Isosceles triangle")
+     document.getElementById("This is an Isosceles triangle")
    }
 
    else if (length1!==length2 && length2!==length3 && length1!==length3){
-     alert("This is a Scalene triangle")
+     document.getElementById("This is a Scalene triangle")
 
    }
 
